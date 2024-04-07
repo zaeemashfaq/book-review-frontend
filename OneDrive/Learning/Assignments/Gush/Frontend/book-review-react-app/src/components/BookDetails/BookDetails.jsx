841c7ac3b2a91393b7259287ce04e5c0b427a4a1
Zaeem Ashfaq <zaeemashfaq@gmail.com>
2024-04-07 15:40:17 +0530

    User review backend integrated with frontend and tested.

--- OneDrive/Learning/Assignments/Gush/Frontend/book-review-react-app/src/components/BookDetails/BookDetails.jsx
+++ OneDrive/Learning/Assignments/Gush/Frontend/book-review-react-app/src/components/BookDetails/BookDetails.jsx
@@ -6,49 +6,10 @@ import { useParams } from "react-router-dom";
 
 function BookDetails() {
   const { id } = useParams();
-  /*
-  const book = {
-    id: 1,
-    title: "To Kill a Mockingbird",
-    description: "One of the classic. Once you start reading, you can't put down the book before finishing",
-    author: "Harper Lee",
-    book_cover_image: "https://fakeimg.pl/667x1000/cc6600",
-    overall_rating: 4, // Overall rating value (integer)
-    user_reviews: [
-      {
-        username: "user1",
-        user_rating: 5,
-        user_review_title: "A Must-Read!",
-        user_review: "Excellent book, a must-read!"
-      },
-      {
-        username: "user2",
-        user_rating: 4,
-        user_review_title: "Enjoyable",
-        user_review: "Really enjoyed it."
-      },
-      {
-        username: "user3",
-        user_rating: 3,
-        user_review_title: "Just Okay",
-        user_review: "It was okay, not my favorite."
-      },
-      {
-        username: "user4",
-        user_rating: 4,
-        user_review_title: "Great Storytelling",
-        user_review: "Great storytelling."
-      },
-      {
-        username: "user5",
-        user_rating: 5,
-        user_review_title: "One of the Best",
-        user_review: "One of the best books I've read."
-      }
-    ]
-  };*/
+  
   const [isLoading, setIsLoading] = useState(true);
   const [book, setBook] = useState({});
+
   useEffect(() => {
     fetch(`http://localhost:5000/books/${id}`)
       .then(function (res) {
@@ -60,7 +21,6 @@ function BookDetails() {
         setIsLoading(false);
       });
   }, [id]);
-  // const { id, title, description, author, book_cover_image, overall_rating, user_reviews } = book;
   if (isLoading) {
     return (
       <div className="Loader">
@@ -69,28 +29,6 @@ function BookDetails() {
     );
   }
   else {
-    /*
-    return (
-      <div className="BookDetails">
-        <div className="Details">
-          <div className="Cover">
-            <img src={book_cover_image} alt={title} />
-          </div>
-          <div className="BookInfo">
-            <h2 className="Title">{title}</h2>
-            <h3 className="Author">{author}</h3>
-            <p className="Description">{description}</p>
-            <RatingComponent rating={overall_rating} />
-          </div>
-        </div>
-        <div className="Reviews">
-          <h3>User Reviews:</h3>
-          {user_reviews.map((user_review, index) => (
-            <UserReview key={index} user_review={user_review} />
-          ))}
-        </div>
-      </div>
-    );*/
     return (
       <div className="BookDetails">
         <div className="Details">
@@ -103,6 +41,12 @@ function BookDetails() {
             <p className="Description">{book.description}</p>
             <RatingComponent rating={book.overall_rating} />
           </div>
+        </div>
+        <div className="Reviews">
+          <h3>User Reviews:</h3>
+          {book.user_reviews.map((user_review, index) => (
+            <UserReview key={index} user_review={user_review} />
+          ))}
         </div>
       </div>
     );
--- OneDrive/Learning/Assignments/Gush/Frontend/book-review-react-app/src/components/UserReview/UserReview.jsx
+++ OneDrive/Learning/Assignments/Gush/Frontend/book-review-react-app/src/components/UserReview/UserReview.jsx
@@ -2,15 +2,15 @@ import "./UserReview.css";
 import RatingComponent from "../RatingComponent";
 
 function UserReview({user_review}){
-    const  {username, user_rating, user_review_title, user_review_content } = user_review;
+    // const  {username, user_rating, user_review_title, user_review_content } = user_review;
     return(
         <div className="UserReview">
             <div className="UserInfo">
-                <p>{username}</p>
-                <RatingComponent rating={user_rating} />
-                <div className="ReviewTitle">{user_review_title}</div>
+                <p>{user_review.username}</p>
+                <RatingComponent rating={user_review.review_rating} />
+                <div className="ReviewTitle">{user_review.review_title}</div>
             </div>
-            <div className="Review">{user_review_content}</div>
+            <div className="Review">{user_review.review_detail}</div>
         </div>
     );
 }
